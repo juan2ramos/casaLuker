7bd99327c58cc417fe8232789ae721e8b21dc74d
Juan Ramos <juan2ramos@gmail.com>
2015-09-18 15:52:19 -0500

    changes

--- wp-content/themes/luker/page-luker-derivados.php
+++ wp-content/themes/luker/page-luker-derivados.php
@@ -1,7 +1,130 @@
-<?php get_header();?>
-    <main class="Main">
+<?php get_header(); ?>
+    <main class="Main Products">
         <?php include TEMPLATEPATH . '/inc/Products.php' ?>
 
-        <?php the_content() ?>
+        <section class="Products">
+            <article class="LukerCacao-article">
+                <figure><img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-maracas.png" alt="">
+                </figure>
+                <h2>Derivados del Cacao</h2>
+
+                <p>
+                    La presencia de CasaLuker en toda la cadena del cacao, nos permite seleccionar cuidadosamente los
+                    mejores granos provenientes del Cacao Fino de Aroma, garantizando altos estándares de calidad, aroma
+                    y sabor. <br>
+                    Manufacturamos los mejores productos semi-elaborados con calidad Fino de Aroma tales como: masa de
+                    cacao, manteca de cacao, cacao en polvo y cacao en grano.
+                </p>
+            </article>
+            <article>
+                <table id="table-derivados" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                    <tr>
+                        <td style="border-bottom: 1px solid #fff;" colspan="3" bgcolor="#5e0725" width="571" height="16">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 1px solid #fff; border-right: 1px solid #fff;" bgcolor="#3b1018" width="247" height="40">
+                            <p style="color: #fff; font-size: 11px; text-align: center;">DESCRIPCIÓN</p>
+                        </td>
+                        <td style="border-bottom: 1px solid #fff; border-right: 1px solid #fff;" bgcolor="#3b1018" width="170">
+                            <p style="color: #fff; font-size: 11px; text-align: center;">EMBALAJE + FORMATO</p>
+                        </td>
+                        <td style="border-bottom: 1px solid #fff;" bgcolor="#3b1018" width="154">
+                            <p style="color: #fff; font-size: 11px; text-align: center;">VIDA ÚTIL<br>(MESES)</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;" height="38">
+                            <p style="color: #fff; font-size: 12px; text-align: left; margin-left: 15px;">Masa de Cacao</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">Caja x 22 Kg.Discos</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">24</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;" height="37">
+                            <p style="color: #fff; font-size: 12px; text-align: left; margin-left: 15px;">100% Cacao Natural en Polvo, 12%</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">Saco x 25 Kg</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">24</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;" height="33">
+                            <p style="color: #fff; font-size: 12px; text-align: left; margin-left: 15px;">100% Cacao Natural en Polvo, 16%</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">Saco x 25 Kg</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">24</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;" height="34">
+                            <p style="color: #fff; font-size: 12px; text-align: left; margin-left: 15px;">100% Cacao Natural en Polvo, 22%</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">Saco x 25 Kg</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">24</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;" height="33">
+                            <p style="color: #fff; font-size: 12px; text-align: left; margin-left: 15px;">Manteca de Cacao</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">Saco x 25 Kg</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">24</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;" height="34">
+                            <p style="color: #fff; font-size: 12px; text-align: left; margin-left: 15px;">Cacao en grano Origen Colombia</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">Saco x 69 Kg</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">12</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;" height="35">
+                            <p style="color: #fff; font-size: 12px; text-align: left; margin-left: 15px;">Cacao en grano Origen Ecuador</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff; border-right: 1px solid #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">Saco x 69 Kg</p>
+                        </td>
+                        <td style="border-bottom: 1px dashed #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">12</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 1px solid #fff; border-right: 1px solid #fff;" height="38">
+                            <p style="color: #fff; font-size: 12px; text-align: left; margin-left: 15px;">Cacao en grano Origen Perú</p>
+                        </td>
+                        <td style="border-bottom: 1px solid #fff; border-right: 1px solid #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">Saco x 69 Kg</p>
+                        </td>
+                        <td style="border-bottom: 1px solid #fff;">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">12</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </article>
+        </section>
     </main>
-<?php get_footer();?>
+<?php get_footer(); ?>
