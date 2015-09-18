5cc374f35c909455e5fd6018d22cadbc1be40f2b
Juan Ramos <juan2ramos@gmail.com>
2015-09-18 15:38:23 -0500

    changes

--- wp-content/themes/luker/page-luker-maracas.php
+++ wp-content/themes/luker/page-luker-maracas.php
@@ -1,7 +1,65 @@
-<?php get_header();?>
+<?php get_header(); ?>
     <main class="Main">
         <?php include TEMPLATEPATH . '/inc/Products.php' ?>
 
-        <?php the_content() ?>
+        <section>
+            <article class="LukerCacao-article">
+                <figure><img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-cacao.png" alt="">
+                </figure>
+                <h2>Grageas con Centros Tropicales</h2>
+
+                <p>
+                    Como aquel instrumento mágico y seductivo que caracteriza el ambiente alegre y festivo del trópico
+                    con su ritmo dulce y diverso, Luker Maracas trae una nueva gama de grageas de chocolate. Estos
+                    centros de café tostado, cacao tostado y dulces frutas tropicales están cubiertos con nuestro
+                    chocolate Fino de Aroma. <br>
+                    Luker Maracas, Tropical Dragees son grageas ideales para decorar postres y tortas, como inclusión en
+                    preparaciones, topping para helados o simplemente para darle un toque especial a sus recetas.
+                    También pueden acompañar cafés y bebidas calientes, mezclarse con cereales y frutos secos para
+                    elaborar snacks saludables.
+                </p>
+            </article>
+            <article>
+                <figure><img src="<?php bloginfo('template_url') ?>/assets/images/productos/empaque-maracas.png"
+                             alt="">
+                </figure>
+
+                <table id="table-luker-cacao" class="table-luker-cacao" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                    <tr>
+                        <td style="border-bottom: 3px solid #fff;" colspan="5" width="540" height="40">
+                            <p style="color: #fff; font-size: 12px; text-align: left;">&gt;&gt;TODAS NUESTRAS COBERTURAS
+                                REQUIEREN ATEMPERADO</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td rowspan="2"><img src="/images/luker-cacao_02.png" alt="" width="1" height="50">
+                        </td>
+                        <td style="border-bottom: 3px solid #fff; border-right: 1px solid #fff;" rowspan="2"
+                            bgcolor="#3b1018" width="137" height="50">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">EMPAQUE</p>
+                        </td>
+                        <td style="border-bottom: 3px solid #fff; border-right: 1px solid #fff;" rowspan="2"
+                            width="217">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">Bolsa 1 Kg /2,5 Kg/<br>Caja 20
+                                Kg, Discos</p>
+                        </td>
+                        <td style="background-image: url('/images/cinco-gotas.png'); background-repeat: no-repeat; background-position: center; border-bottom: 1px dashed #fff; border-right: 1px solid #fff;">
+                            &nbsp;</td>
+                        <td style="border-bottom: 1px dashed #fff;" width="108" height="27">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">MUY FLUIDO</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-bottom: 3px solid #fff; background-image: url('/images/una-gotas.png'); background-repeat: no-repeat; background-position: center; border-right: 1px solid #fff;"
+                            width="77" height="23">&nbsp;</td>
+                        <td style="border-bottom: 3px solid #fff;" height="23">
+                            <p style="color: #fff; font-size: 12px; text-align: center;">POCO FLUIDO</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </article>
+        </section>
     </main>
-<?php get_footer();?>
+<?php get_footer(); ?>
