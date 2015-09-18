6500f42173541fa54b13bad86976ac9e380aa119
Juan Ramos <juan2ramos@gmail.com>
2015-09-18 16:12:50 -0500

    changes

--- wp-content/themes/luker/page-cobertura-de-chocolate.php
+++ wp-content/themes/luker/page-cobertura-de-chocolate.php
@@ -5,7 +5,7 @@
         <section>
             <article class="LukerCacao-article">
                 <figure><img
-                        src="<?php bloginfo('template_url') ?>/assets/images/productos/cobertura-sabor-a-chocolates.png"
+                        src="<?php bloginfo('template_url') ?>/assets/images/productos/coberturas-sabor-a-chocolates.png"
                         alt="">
                 </figure>
                 <h2>Cobertura Sabor a Chocolates</h2>
@@ -24,41 +24,7 @@
                         alt="">
                 </figure>
 
-                <table id="table-luker-cacao" class="table-luker-cacao" border="0" cellspacing="0" cellpadding="0">
-                    <tbody>
-                    <tr>
-                        <td style="border-bottom: 3px solid #fff;" colspan="5" width="540" height="40">
-                            <p style="color: #fff; font-size: 12px; text-align: left;">&gt;&gt;TODAS NUESTRAS COBERTURAS
-                                REQUIEREN ATEMPERADO</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td rowspan="2"><img src="/images/luker-cacao_02.png" alt="" width="1" height="50">
-                        </td>
-                        <td style="border-bottom: 3px solid #fff; border-right: 1px solid #fff;" rowspan="2"
-                            bgcolor="#3b1018" width="137" height="50">
-                            <p style="color: #fff; font-size: 12px; text-align: center;">EMPAQUE</p>
-                        </td>
-                        <td style="border-bottom: 3px solid #fff; border-right: 1px solid #fff;" rowspan="2"
-                            width="217">
-                            <p style="color: #fff; font-size: 12px; text-align: center;">Bolsa 1 Kg /2,5 Kg/<br>Caja 20
-                                Kg, Discos</p>
-                        </td>
-                        <td style="background-image: url('/images/cinco-gotas.png'); background-repeat: no-repeat; background-position: center; border-bottom: 1px dashed #fff; border-right: 1px solid #fff;">
-                            &nbsp;</td>
-                        <td style="border-bottom: 1px dashed #fff;" width="108" height="27">
-                            <p style="color: #fff; font-size: 12px; text-align: center;">MUY FLUIDO</p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style="border-bottom: 3px solid #fff; background-image: url('/images/una-gotas.png'); background-repeat: no-repeat; background-position: center; border-right: 1px solid #fff;"
-                            width="77" height="23">&nbsp;</td>
-                        <td style="border-bottom: 3px solid #fff;" height="23">
-                            <p style="color: #fff; font-size: 12px; text-align: center;">POCO FLUIDO</p>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+
             </article>
         </section>
     </main>
