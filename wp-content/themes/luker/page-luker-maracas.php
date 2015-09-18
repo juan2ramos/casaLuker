37babe0a0c2065cae10217f9b7b9dc1da496a88b
Juan Ramos <juan2ramos@gmail.com>
2015-09-18 17:43:22 -0500

    changes

--- wp-content/themes/luker/page-luker-maracas.php
+++ wp-content/themes/luker/page-luker-maracas.php
@@ -26,7 +26,9 @@
 
             </article>
         </section>
+
         <div class="bloquetexto"><p></p>
+
             <div id="tabla-maracas">
                 <div id="barra-horizontal">&nbsp;</div>
                 <ol id="titulos">
@@ -35,48 +37,105 @@
                     <li>Embalaje</li>
                 </ol>
                 <div id="primer-grajea">
-                    <p id="producto"><!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p><ul class="sige"><li class="sige_cont_0"><span class="sige_thumb"><a href="http://www.lukeringredients.com/images/grajea1/dark-chocolate-covered-nibs-cluster.jpg" rel="lyteshow.sig0" title="<strong><em>dark-chocolate-covered-nibs-cluster</em></strong>"><img alt="dark-chocolate-covered-nibs-cluster" title="dark-chocolate-covered-nibs-cluster" src="http://www.lukeringredients.com/images/grajea1/thumbs/dark-chocolate-covered-nibs-cluster.jpg"></a></span></li></ul>
+                    <p id="producto">
+                        <!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p>
+                    <ul class="sige">
+                        <li class="sige_cont_0"><span class="sige_thumb"><a
+                                    href="http://www.lukeringredients.com/images/grajea1/dark-chocolate-covered-nibs-cluster.jpg"
+                                    rel="lyteshow.sig0"
+                                    title="<strong><em>dark-chocolate-covered-nibs-cluster</em></strong>"><img
+                                        alt="dark-chocolate-covered-nibs-cluster"
+                                        title="dark-chocolate-covered-nibs-cluster"
+                                        src="http://www.lukeringredients.com/images/grajea1/thumbs/dark-chocolate-covered-nibs-cluster.jpg"></a></span>
+                        </li>
+                    </ul>
                     <span class="sige_clr"></span>
-                    <!--<a href="/images/grajea1.jpg" rel="prettyPhoto" title="Cerrar">cerrar<img id="producto" src="/images/grajea1.jpg" /></a>--><ol id="name">
+                    <!--<a href="/images/grajea1.jpg" rel="prettyPhoto" title="Cerrar">cerrar<img id="producto" src="/images/grajea1.jpg" /></a>-->
+                    <ol id="name">
                         <li>G501</li>
                         <li>Dark Chocolate</li>
                         <li>Covered Nibs Cluster</li>
                     </ol>
-                    <p id="descripcion">Pequeños trozos del mejor Cacao Fino de Aroma tostados y agrupados son cubiertos con chocolate oscuro formando un delicioso snack crocante con mucho sabor a cacao. Ideal como snack, topping e inclusión en sus recetas.</p>
+                    <p id="descripcion">Pequeños trozos del mejor Cacao Fino de Aroma tostados y agrupados son cubiertos
+                        con chocolate oscuro formando un delicioso snack crocante con mucho sabor a cacao. Ideal como
+                        snack, topping e inclusión en sus recetas.</p>
                     <ol id="packaging">
                         <li>Bolsa 1 Kg</li>
                         <li>Caja 15 Kg</li>
-                    </ol><!--<p id="vida-util">24</p>--></div>
+                    </ol>
+                    <!--<p id="vida-util">24</p>--></div>
                 <div id="primer-grajea">
-                    <p id="producto"><!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p><ul class="sige"><li class="sige_cont_1"><span class="sige_thumb"><a href="http://www.lukeringredients.com/images/grajea2/dark-chocolate-covered-nibs.jpg" rel="lyteshow.sig1" title="<strong><em>dark-chocolate-covered-nibs</em></strong>"><img alt="dark-chocolate-covered-nibs" title="dark-chocolate-covered-nibs" src="http://www.lukeringredients.com/images/grajea2/thumbs/dark-chocolate-covered-nibs.jpg"></a></span></li></ul>
+                    <p id="producto">
+                        <!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p>
+                    <ul class="sige">
+                        <li class="sige_cont_1"><span class="sige_thumb"><a
+                                    href="http://www.lukeringredients.com/images/grajea2/dark-chocolate-covered-nibs.jpg"
+                                    rel="lyteshow.sig1"
+                                    title="<strong><em>dark-chocolate-covered-nibs</em></strong>"><img
+                                        alt="dark-chocolate-covered-nibs" title="dark-chocolate-covered-nibs"
+                                        src="http://www.lukeringredients.com/images/grajea2/thumbs/dark-chocolate-covered-nibs.jpg"></a></span>
+                        </li>
+                    </ul>
                     <span class="sige_clr"></span>
-                    <!--<a href="/images/grajea2.jpg" rel="prettyPhoto"><img id="producto" src="/images/grajea2.jpg" /></a>--><ol id="name">
+                    <!--<a href="/images/grajea2.jpg" rel="prettyPhoto"><img id="producto" src="/images/grajea2.jpg" /></a>-->
+                    <ol id="name">
                         <li>G502</li>
                         <li>Dark Chocolate</li>
                         <li>Covered Nibs</li>
                     </ol>
-                    <p id="descripcion">Pequeños trozos del mejor Cacao Fino de Aroma, son tostados y luego son cubiertos con chocolate oscuro. Ideales para consumir como snack, para inclusiones en sus postres y decoraciones.</p>
+                    <p id="descripcion">Pequeños trozos del mejor Cacao Fino de Aroma, son tostados y luego son
+                        cubiertos con chocolate oscuro. Ideales para consumir como snack, para inclusiones en sus
+                        postres y decoraciones.</p>
                     <ol id="packaging">
                         <li>Bolsa 1Kg</li>
                         <li>Caja 10 Kg</li>
-                    </ol><!--<p id="vida-util">24</p>--></div>
+                    </ol>
+                    <!--<p id="vida-util">24</p>--></div>
                 <div id="primer-grajea">
-                    <p id="producto"><!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p><ul class="sige"><li class="sige_cont_2"><span class="sige_thumb"><a href="http://www.lukeringredients.com/images/grajea3/dark-chocolate-covered-espresso-beans.jpg" rel="lyteshow.sig2" title="<strong><em>dark-chocolate-covered-espresso-beans</em></strong>"><img alt="dark-chocolate-covered-espresso-beans" title="dark-chocolate-covered-espresso-beans" src="http://www.lukeringredients.com/images/grajea3/thumbs/dark-chocolate-covered-espresso-beans.jpg"></a></span></li></ul>
+                    <p id="producto">
+                        <!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p>
+                    <ul class="sige">
+                        <li class="sige_cont_2"><span class="sige_thumb"><a
+                                    href="http://www.lukeringredients.com/images/grajea3/dark-chocolate-covered-espresso-beans.jpg"
+                                    rel="lyteshow.sig2"
+                                    title="<strong><em>dark-chocolate-covered-espresso-beans</em></strong>"><img
+                                        alt="dark-chocolate-covered-espresso-beans"
+                                        title="dark-chocolate-covered-espresso-beans"
+                                        src="http://www.lukeringredients.com/images/grajea3/thumbs/dark-chocolate-covered-espresso-beans.jpg"></a></span>
+                        </li>
+                    </ul>
                     <span class="sige_clr"></span>
-                    <!--<a href="/images/grajea3.jpg" rel="prettyPhoto"><img id="producto" src="/images/grajea3.jpg" /></a>--><ol id="name">
+                    <!--<a href="/images/grajea3.jpg" rel="prettyPhoto"><img id="producto" src="/images/grajea3.jpg" /></a>-->
+                    <ol id="name">
                         <li>G503</li>
                         <li>Dark Chocolate</li>
                         <li>Covered Espresso Beans</li>
                     </ol>
-                    <p id="descripcion">La combinación perfecta. Los granos del mejor café colombiano cubiertos con generosas capas de chocolate Fino de Aroma. Su brillo y forma ovalada los hacen diferentes a todos los de su clase. Ideal para acompañar un café, como snack e inclusión en sus recetas y decoraciones.</p>
+                    <p id="descripcion">La combinación perfecta. Los granos del mejor café colombiano cubiertos con
+                        generosas capas de chocolate Fino de Aroma. Su brillo y forma ovalada los hacen diferentes a
+                        todos los de su clase. Ideal para acompañar un café, como snack e inclusión en sus recetas y
+                        decoraciones.</p>
                     <ol id="packaging">
                         <li>Bolsa 1 Kg</li>
                         <li>Caja 15 Kg</li>
-                    </ol><!--<p id="vida-util">24</p>--></div>
+                    </ol>
+                    <!--<p id="vida-util">24</p>--></div>
                 <div id="primer-grajea">
-                    <p id="producto"><!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p><ul class="sige"><li class="sige_cont_3"><span class="sige_thumb"><a href="http://www.lukeringredients.com/images/grajea4/dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee.jpg" rel="lyteshow.sig3" title="<strong><em>dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee</em></strong>"><img alt="dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee" title="dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee" src="http://www.lukeringredients.com/images/grajea4/thumbs/dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee.jpg"></a></span></li></ul>
+                    <p id="producto">
+                        <!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p>
+                    <ul class="sige">
+                        <li class="sige_cont_3"><span class="sige_thumb"><a
+                                    href="http://www.lukeringredients.com/images/grajea4/dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee.jpg"
+                                    rel="lyteshow.sig3"
+                                    title="<strong><em>dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee</em></strong>"><img
+                                        alt="dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee"
+                                        title="dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee"
+                                        src="http://www.lukeringredients.com/images/grajea4/thumbs/dark-chocolate-covered-instant-coffee_dark-chocolate-covered-extra-instant-coffee.jpg"></a></span>
+                        </li>
+                    </ul>
                     <span class="sige_clr"></span>
-                    <!--<a href="/images/grajea4.jpg" rel="prettyPhoto"><img id="producto" src="/images/grajea4.jpg" /></a>--><ol id="name4">
+                    <!--<a href="/images/grajea4.jpg" rel="prettyPhoto"><img id="producto" src="/images/grajea4.jpg" /></a>-->
+                    <ol id="name4">
                         <li>G504</li>
                         <li>Dark Chocolate</li>
                         <li>Covered Instant Coffee</li>
@@ -85,30 +144,55 @@
                         <li>Dark chocolate</li>
                         <li>Covered extra instant coffee</li>
                     </ol>
-                    <p id="descripcion">El sabor explosivo del café logra el balance perfecto de esta gragea. Este toque único de sabor hará que sus productos se diferencien y sorprendan a quienes los prueban. Ideal en sus decoraciones y postres o como topping en heladería.</p>
+                    <p id="descripcion">El sabor explosivo del café logra el balance perfecto de esta gragea. Este toque
+                        único de sabor hará que sus productos se diferencien y sorprendan a quienes los prueban. Ideal
+                        en sus decoraciones y postres o como topping en heladería.</p>
                     <ol id="packaging">
                         <li>Bolsa 1 Kg</li>
                         <li>Caja 10 Kg</li>
-                    </ol><!--<p id="vida-util">24</p>--></div>
+                    </ol>
+                    <!--<p id="vida-util">24</p>--></div>
                 <div id="primer-grajea">
-                    <p id="producto"><!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p><ul class="sige"><li class="sige_cont_4"><span class="sige_thumb"><a href="http://www.lukeringredients.com/images/grajea5/dark-chocolate-covered-physalis.jpg" rel="lyteshow.sig4" title="<strong><em>dark-chocolate-covered-physalis</em></strong>"><img alt="dark-chocolate-covered-physalis" title="dark-chocolate-covered-physalis" src="http://www.lukeringredients.com/images/grajea5/thumbs/dark-chocolate-covered-physalis.jpg"></a></span></li></ul>
+                    <p id="producto">
+                        <!-- Simple Image Gallery Extended - Plugin Joomla! 2.5 by Kubik-Rubik.de Viktor Vogel --></p>
+                    <ul class="sige">
+                        <li class="sige_cont_4"><span class="sige_thumb"><a
+                                    href="http://www.lukeringredients.com/images/grajea5/dark-chocolate-covered-physalis.jpg"
+                                    rel="lyteshow.sig4"
+                                    title="<strong><em>dark-chocolate-covered-physalis</em></strong>"><img
+                                        alt="dark-chocolate-covered-physalis" title="dark-chocolate-covered-physalis"
+                                        src="http://www.lukeringredients.com/images/grajea5/thumbs/dark-chocolate-covered-physalis.jpg"></a></span>
+                        </li>
+                    </ul>
                     <span class="sige_clr"></span>
-                    <!--<a href="/images/grajea5.jpg" rel="prettyPhoto"><img id="producto" src="/images/grajea5.jpg" /></a>--><ol id="name">
+                    <!--<a href="/images/grajea5.jpg" rel="prettyPhoto"><img id="producto" src="/images/grajea5.jpg" /></a>-->
+                    <ol id="name">
                         <li>G505</li>
                         <li>Dark Chocolate</li>
                         <li>Covered Physalis</li>
                     </ol>
-                    <p id="descripcion">Esta fruta tropical, conocida como uchuva, es recubierta con chocolate oscuro buscando el equilibrio ideal entre el sabor ácido de la fruta y las notas frutales y florales del chocolate. Ideal como decoración y snack.</p>
+                    <p id="descripcion">Esta fruta tropical, conocida como uchuva, es recubierta con chocolate oscuro
+                        buscando el equilibrio ideal entre el sabor ácido de la fruta y las notas frutales y florales
+                        del chocolate. Ideal como decoración y snack.</p>
                     <ol id="packaging">
                         <li>Bolsa 1 Kg</li>
                         <li>Caja 20 Kg</li>
-                    </ol><!--<p id="vida-util">12</p>--></div>
-                <div id="vida-util"><ol>
+                    </ol>
+                    <!--<p id="vida-util">12</p>--></div>
+                <div id="vida-util">
+                    <ol>
                         <li>Vida útil (Meses)</li>
                         <li class="dos">24</li>
-                    </ol></div>
+                    </ol>
+                </div>
             </div>
-            <p></p><div class="contenido-img"><img src="/images/manteca-cacao.png" alt="" height="114" width="115">&nbsp; &nbsp; &nbsp; &nbsp;<img src="/images/Koshercertification.png" alt="Kosher-Certification" height="101" width="115"></div><p></p>
+            <p></p>
+
+            <div class="contenido-img"><img src="/images/manteca-cacao.png" alt="" height="114" width="115">&nbsp;
+                &nbsp; &nbsp; &nbsp;<img src="/images/Koshercertification.png" alt="Kosher-Certification" height="101"
+                                         width="115"></div>
+            <p></p>
+
             <p></p></div>
     </main>
 <?php get_footer(); ?>
