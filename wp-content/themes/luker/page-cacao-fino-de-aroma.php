7a6c1c3e1690ad7c9461e7f3df7476ef8a2c7b6b
Juan Ramos <juan2ramos@gmail.com>
2015-09-25 19:20:28 -0500

    cacao fino de aroma

--- wp-content/themes/luker/page-cacao-fino-de-aroma.php
+++ wp-content/themes/luker/page-cacao-fino-de-aroma.php
@@ -1,5 +1,148 @@
-<?php get_header();?>
-<main>
+<?php get_header(); ?>
+    <main class=" Main Products">
+        <section>
+            <article>
+                <figure><img src="<?php bloginfo('template_url') ?>/assets/images/luker-logo.png" alt="">
+                </figure>
+                <h2>CACAO FINO DE AROMA </h2>
 
-</main>
-<?php get_footer();?>
+                <p>
+                    Aromas y sabores frutales, florales, de nueces y de malta. Este es el sabor característico del Cacao
+                    Fino de Aroma que lo diferencia de los demás cacaos en el mundo.
+                </p>
+
+                <p>
+                    Fino de Aroma es una clasificación de la Organización Internacional del Cacao (ICCO) que describe un
+                    cacao de exquisito aroma y sabor. Este tipo de cacao representa alrededor del 8% de la producción de
+                    cacao en el mundo. En Colombia, Ecuador, Venezuela y Perú se produce el 76% del Cacao Fino de Aroma
+                    del mundo.
+                </p>
+            </article>
+            <article>
+                <h2>CRIOLLOS Y TRINITARIOS </h2>
+
+                <p>
+                    Los cacaos de este tipo provienen en su mayoría de cacaos criollos y trinitarios, que son diferentes
+                    en términos de aroma y sabor a los cacaos de tipo forastero.
+                    Los cacaos trinitarios, muy apetecidos en el mundo, son Cacaos Finos de Aroma, de sabor suave y
+                    aromático. La variedad trinitario es el resultado del cruzamiento de las variedades de tipo criollo
+                    y forastero y son producidos principalmente en Colombia, Ecuador, Perú y Venezuela.
+                    El cacao forastero es el más común en el mundo, es resistente al clima y su sabor es ácido y
+                    astringente. Este es el típico cacao de África.
+                </p>
+
+                <figure><img
+                        src="<?php bloginfo('template_url') ?>/assets/images/img-criollos-trinitario.png"
+                        alt="" "="">
+                </figure>
+            </article>
+        </section>
+        <section>
+            <article>
+
+                <iframe width="100%" class="iframe" height="178" src="https://www.youtube.com/embed/KrAOCWrUGAc"
+                        frameborder="0"
+                        allowfullscreen></iframe>
+                <p>
+                    En CasaLuker compramos el 38% de la producción total de cacao en Colombia – 1600 toneladas al mes en
+                    promedio –.
+
+                </p>
+            </article>
+
+            <article class="city">
+                <img src="<?php bloginfo('template_url') ?>/assets/images/desgranado.jpg" alt="">
+                <img src="<?php bloginfo('template_url') ?>/assets/images/flor-a-escala.jpg"
+                     alt="">
+                <img src="<?php bloginfo('template_url') ?>/assets/images/grano-bien-fermentado.jpg" alt="">
+                <img src="<?php bloginfo('template_url') ?>/assets/images/mazorcas.jpg" alt="">
+            </article>
+
+        </section>
+        <section>
+            <article>
+
+                <h2>INVESTIGACIÓN CASALUKER</h2>
+
+                <p>
+                    CasaLuker en alianza con cenicafé* - uno de los institutos de investigación acerca del café más
+                    importantes en el mundo – inició en el año 2005 el proceso de caracterización de los cacaos en las
+                    principales zonas productoras de Colombia, Ecuador, Venezuela y de otras regiones cacaoteras
+                    alrededor del mundo.
+                </p>
+
+                <p>
+                    En esta investigación se han estudiado las características sensoriales de cada tipo de cacao, se han
+                    identificado y cuantificado los principales compuestos volátiles del aroma y otras características
+                    físicas, químicas, nutricionales y funcionales que permiten definir una huella digital para cada
+                    tipo de cacao.
+                </p>
+
+                <p>
+                    CasaLuker por su estrecha relación con los agricultures y sus continuos programas de capacitación
+                    controla estos factores para ofrecer a sus clientes el mejor sabor.
+                </p>
+            </article>
+            <article>
+                <ul class="TabsMenu">
+                    <li class="active"><img src="<?php bloginfo('template_url') ?>/assets/images/fermentacion.png"
+                                            alt=""></li>
+                    <li><img src="<?php bloginfo('template_url') ?>/assets/images/secado.png" alt=""></li>
+                    <li><img src="<?php bloginfo('template_url') ?>/assets/images/tostion.png" alt=""></li>
+                    <li><img src="<?php bloginfo('template_url') ?>/assets/images/concheo.png" alt=""></li>
+                </ul>
+
+                <div class="Tabs">
+                    <ul>
+                        <li>
+                            <p>
+                                El secado es un proceso gradual realizado en el campo por el agricultor. Durante este
+                                proceso se exponen los granos al sol durante varios días para bajar el contenido de agua
+                                del grano del 55% al 7% y eliminar algunos ácidos naturalmente presentes en el cacao.
+                            </p>
+                            <p>
+                                Un buen secado es clave para evitar mohos y hongos en el grano y garantizar un buen
+                                sabor.
+                            </p>
+                            <figure>
+                                <img src="<?php bloginfo('template_url') ?>/assets/images/procesos-img-secado.jpg" alt="">
+                            </figure>
+                        </li>
+                    </ul>
+                </div>
+
+            </article>
+        </section>
+        <section>
+            <article>
+
+                <h2>INVESTIGACIÓN CASALUKER</h2>
+
+                <p>
+                    CasaLuker en alianza con cenicafé* - uno de los institutos de investigación acerca del café más
+                    importantes en el mundo – inició en el año 2005 el proceso de caracterización de los cacaos en las
+                    principales zonas productoras de Colombia, Ecuador, Venezuela y de otras regiones cacaoteras
+                    alrededor del mundo.
+                </p>
+
+                <p>
+                    En esta investigación se han estudiado las características sensoriales de cada tipo de cacao, se han
+                    identificado y cuantificado los principales compuestos volátiles del aroma y otras características
+                    físicas, químicas, nutricionales y funcionales que permiten definir una huella digital para cada
+                    tipo de cacao.
+                </p>
+                <figure class="Cenicafe">
+                    <img src="<?php bloginfo('template_url') ?>/assets/images/cenicafe.jpeg" alt="">
+
+                </figure>
+                    <span class="Cenicafe-span">
+                        *Centro Nacional de Investigaciones de Café
+                    </span>
+            </article>
+            <article>
+                <figure><img src="<?php bloginfo('template_url') ?>/assets/images/grafica-investigaicon.jpg" alt="">
+                </figure>
+            </article>
+        </section>
+    </main>
+<?php get_footer(); ?>
