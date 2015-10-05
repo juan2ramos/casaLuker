49ab74b10dfd653707f8890bff4be500f5a16081
Juan Ramos <juan2ramos@gmail.com>
2015-10-05 18:13:00 -0500

    taxonomias

--- wp-content/themes/luker/page-recetas.php
+++ wp-content/themes/luker/page-recetas.php
@@ -4,12 +4,16 @@
 
         <?php
 
-
-        $args = array('post_type' => 'recipe','order'   => 'DESC',);
+        $args = array('post_type' => 'recipe', 'order' => 'DESC',);
         $query = new WP_Query($args);
 
         while ($query->have_posts()) : $query->the_post(); // print_r(get_the_terms($post->ID, 'cacao'));?>
-            <div class="Recipes-box mix <?php print_r(get_the_terms($post->ID, 'cacao')); print_r(get_the_terms($post->ID, 'chef')); print_r(get_the_terms($post->ID, 'level')); ?> ">
+            <?php
+            $cacao = get_the_terms($post->ID, 'cacao')[0]->slug;
+            $chef = get_the_terms($post->ID, 'chef')[0]->slug;
+            $level = get_the_terms($post->ID, 'level')[0]->slug;
+            ?>
+            <div class="Recipes-box mix <?php echo $cacao . " " . $chef . " " . $level ?>">
                 <figure class="Recipes-figure">
                     <?php the_post_thumbnail(); ?>
                 </figure>
@@ -21,7 +25,7 @@
             </div>
         <?php endwhile; ?>
     </main>
-<?php wp_enqueue_script( 'mixitup', 'http://cdn.jsdelivr.net/jquery.mixitup/latest/jquery.mixitup.min.js?v=2.1.2', array('jquery'), 1, false ); ?>
-<?php wp_enqueue_script( 'mixScript', get_template_directory_uri().'/assets/js/mixitup.js', array(''), 1, false ); ?>
+<?php wp_enqueue_script('mixitup', 'http://cdn.jsdelivr.net/jquery.mixitup/latest/jquery.mixitup.min.js?v=2.1.2', array('jquery'), 1, false); ?>
+<?php wp_enqueue_script('mixScript', get_template_directory_uri() . '/assets/js/mixitup.js', array(''), 1, false); ?>
 
 <?php get_footer(); ?>
