6fbfc8bdd17971e4319ec762bcc900a9bef49ffc
Juan Ramos <juan2ramos@gmail.com>
2015-10-05 17:20:22 -0500

    mixitUp

--- wp-content/themes/luker/page-recetas.php
+++ wp-content/themes/luker/page-recetas.php
@@ -9,7 +9,7 @@
         $query = new WP_Query($args);
 
         while ($query->have_posts()) : $query->the_post(); // print_r(get_the_terms($post->ID, 'cacao'));?>
-            <div class="Recipes-box">
+            <div class="Recipes-box mix <?php print_r(get_the_terms($post->ID, 'cacao')); print_r(get_the_terms($post->ID, 'chef')); print_r(get_the_terms($post->ID, 'level')); ?> ">
                 <figure class="Recipes-figure">
                     <?php the_post_thumbnail(); ?>
                 </figure>
@@ -21,4 +21,7 @@
             </div>
         <?php endwhile; ?>
     </main>
+<?php wp_enqueue_script( 'mixitup', 'http://cdn.jsdelivr.net/jquery.mixitup/latest/jquery.mixitup.min.js?v=2.1.2', array('jquery'), 1, false ); ?>
+<?php wp_enqueue_script( 'mixScript', get_template_directory_uri().'/assets/js/mixitup.js', array(''), 1, false ); ?>
+
 <?php get_footer(); ?>
