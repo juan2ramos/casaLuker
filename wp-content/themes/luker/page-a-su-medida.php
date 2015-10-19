851a4da117638f71ce165a54300f2e8292992b9c
Juan Ramos <juan2ramos@gmail.com>
2015-10-19 17:27:10 -0500

    a su medida y caluker

---
+++ wp-content/themes/luker/page-a-su-medida.php
@@ -0,0 +1,70 @@
+<?php get_header(); ?>
+<main class="Main a-su-medida">
+    <section>
+        <article>
+            <h2> INNOVACIÓN A SU MEDIDA</h2>
+
+            <p>
+                En el centro de Investigación y Desarrollo CasaLuker desarrollamos productos a la medida en cooperación
+                con nuestros clientes bajo estándares internacionales.
+            </p>
+
+            <p>
+                Productos a la medida y marcas privadas:
+                <a href="mailto:lukercacao@casaluker.com.co">lukercacao@casaluker.com.co</a>
+            </p>
+
+            <h2 class="top"> CÓMO CATAR MASA DE CACAO</h2>
+
+            <p>
+                La Cata de masa de Cacao es una práctica profesional que se hace para determinar los atributos del aroma
+                y
+                el sabor en una muestra de Cacao específica.
+                El proceso consiste en olfatear y saborear la muestra para medir el nivel de sabor a cacao, la acidez,
+                la
+                astringencia y el nivel amargo, frutal, floral y de nuez, entre otras características.
+            </p>
+
+            <p>
+                El proceso:
+            </p>
+            <p>
+                - Tome la muestra y olfatéela para percibir sus aromas mientras remueve el contenido con una cuchara ó
+                espátula.
+                - Tome una pequeña muestra de masa y llévelo a la boca buscando una dispersión uniforme sobre la lengua.
+                - Mantenga la muestra en su boca durante 15 o 20 segundos, aireando un poco con bocanadas de aire.
+                - Los atributos son evidentes durante este tiempo, escriba sus percepciones con la ayuda de la nariz del
+                cacao CasaLuker.
+            </p>
+            <p>
+                Nota: Es recomendado realizar un enjuague con agua entre muestras.
+            </p>
+        </article>
+        <article>
+            <div>
+                <iframe src="https://www.youtube.com/embed/1oaHPNy3gVI" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div class="cocoa-nose">
+                <div>
+                    <h3>NARIZ DEL CACAO</h3>
+                    <span>PANEL DE CAPACITACIÓN</span>
+                </div>
+                <div>
+                    <h3>QUÌMICO MEDICINAL</h3>
+                    <ul>
+                        <li>Amargo</li>
+                        <li>Cuero</li>
+                        <li>Metálico</li>
+                        <li>Plástico</li>
+                        <li>Caucho</li>
+                        <li>Gasolina</li>
+                    </ul>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </article>
+    </section>
+</main>
+<?php get_footer(); ?>
---
+++ wp-content/themes/luker/page-casaluker.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+<main class="Main casa-luker">
+    <section>
+        <article>
+            <h2> Bienvenidos al Grupo CasaLuker</h2>
+
+            <p>
+                Con la apertura de nuestra filial CasaLuker Europe en Bélgica en el 2009, nos hemos consolidado como un
+                grupo de fuerte proyección internacional que se destaca por su experiencia en la oferta de productos
+                derivados del Cacao Fino de Aroma. Nuestra presencia internacional se ve reforzada por nuestra red de
+                oficinas en Colombia, Rusia, Panamá, Ecuador y Perú.
+            </p>
+
+            <p>
+                La tradición de una empresa familiar fundada en 1906 y el constante trabajo de la mano del agricultor,
+                sumada a la alta inversión en equipos de última generación en la producción de coberturas de chocolates,
+                nos
+                permite ofrecer coberturas de chocolate, licor de cacao, manteca, polvo y grano de cacao que cumplen con
+                los
+                más altos estándares internacionales de calidad.
+            </p>
+
+            <p>
+                CasaLuker, dentro de su compromiso de sostenibilidad del desarrollo social, económico y ambiental de las
+                comunidades cultivadoras de cacao, es miembro de la World Cocoa Foundation WCF. Contamos con las
+                certificaciones internacionales de calidad como son HACCP, ISO 9001,Kosher y BASC.
+            </p>
+
+            <p>
+                Tenemos por objetivo satisfacer las necesidades de nuestros consumidores ofreciéndoles un sabor único y
+                especial. Nuestro compromiso es estar cada vez más cerca de nuestros clientes y aumentar el éxito de sus
+                productos con el continuo asesoramiento y servicio personalizado.
+            </p>
+            <figure>
+                <img src="<?php bloginfo('template_url') ?>/assets/images/logos-de-calidad.png" alt="">
+            </figure>
+        </article>
+        <article>
+            <div>
+                <iframe  src="https://www.youtube.com/embed/DDz_5oo-l7c" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div>
+                <h2> Acerca de nosotros</h2>
+                <iframe  src="https://www.youtube.com/embed/T-njXU_3NFk" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </article>
+    </section>
+</main>
+<?php get_footer(); ?>
