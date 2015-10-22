239a06fe87121c2a0431053d1726743e510e4ae7
Juan Ramos <juan2ramos@gmail.com>
2015-10-21 19:03:25 -0500

    luker way

---
+++ wp-content/themes/luker/page-the-luker-way.php
@@ -0,0 +1,70 @@
+<?php get_header(); ?>
+<main class="Main LukerWay">
+
+    <section class="LukerWay-header">
+        <div class="LukerWay-headerContent">
+            <article>
+                <h2>¿QUÉ ES <br/> THE LUKER WAY?</h2>
+
+                <p>
+                    Es la conexión que tenemos con nuestros socios; es nuestra relación con los agricultores (nuestros
+                    proveedores), el medio ambiente, nuestros empleados, los clientes y la sociedad. Creemos en
+                    compartir
+                    oportunidades, conocimiento, entrenamiento y tecnología, y trabajamos juntos para producir el mejor
+                    Cacao Fino de Aroma.
+                </p>
+            </article>
+            <ul>
+                <li><a href="">NUESTRAS CREENCIAS</a></li>
+                <li><a href="">COMPROMISO SOCIAL</a></li>
+                <li><a href="">PROYECTOS EMPRESARIALES</a></li>
+                <li><a href="">ENFOQUE CIENTÍFICO</a></li>
+            </ul>
+        </div>
+    </section>
+    <section class="LukerWay-cacao">
+
+    </section>
+
+    <section class="LukerWay-term">
+
+    </section>
+    <section class="LukerWay-social">
+
+    </section>
+    <section class="LukerWay-business">
+
+    </section>
+    <section class="LukerWay-scientific">
+        <article class="LukerWay-scientificContent">
+            <div class="middle">
+
+                <h2>ENFOQUE CIENTÍFICO</h2>
+                <p>
+                    Durante los últimos 10 años, CasaLuker ha estado trabajando con Cenicafé en la caracterización y
+                    profundización en el conocimiento del cacao, analizando y comparando diferentes especies de granos
+                    de
+                    cacao
+                    de todo el mundo.
+
+                    <span style=" color: #FD9631; font-weight: bold;margin:1rem 5rem 0">Más de</span>
+                    <span style=" color: #FD9631; margin: 0 7rem; font-weight: bold; font-size: 1.8rem "> <b style="color: white; font-size: 1.8rem">2370</b> muestras</span>
+                    <span style=" color: #FD9631; margin-left: 16rem;">procesadas</span>
+                    <span style=" margin:2.5rem 5rem 0 ; color: #FD9631;">representando</span>
+                    <span style=" color: #FD9631; margin: .1rem 7rem .3rem; font-weight: bold; font-size: 1.8rem "><b style="color: white; font-size: 1.8rem">327060</b></span>
+                    <span style=" color: #FD9631; margin-left: 10rem;">horas de trabajo</span>
+                </p>
+            </div>
+        </article>
+    </section>
+    <section class="socialNetwork">
+        <ul>
+            <li><h3>TWEETS</h3></li>
+            <li><h3>FACEBOOK</h3></li>
+            <li><h3>YOUTUBE</h3></li>
+        </ul>
+    </section>
+
+
+</main>
+<?php get_footer(); ?>
