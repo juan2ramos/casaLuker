b44b0c3772164dac4cf3a7d2ed65c3329b7964d5
Juan Ramos <juan2ramos@gmail.com>
2015-10-26 19:41:04 -0500

    projectBusiness

--- wp-content/themes/luker/page-the-luker-way.php
+++ wp-content/themes/luker/page-the-luker-way.php
@@ -2,7 +2,7 @@
 <main class="Main LukerWay">
     <nav class="LukerWay-nav">
         <div class="menu">
-            <a href="#" >MENU<span>◄</span></a>
+            <a href="#">MENU<span>◄</span></a>
         </div>
         <ul>
             <li><a href="#" data-target="LukerWay-cacao">NUESTRAS CREENCIAS</a></li>
@@ -31,28 +31,63 @@
                 <li><a href="#" data-target="LukerWay-scientific">ENFOQUE CIENTÍFICO</a></li>
             </ul>
         </div>
-        <div class="LukerWay-bar"><span style="display: none" class="LukerWay-barFirst"></span><span data-target="LukerWay-cacao" class="LukerWay-barLast"></span> </div>
+        <div class="LukerWay-bar"><span style="display: none" class="LukerWay-barFirst"></span><span
+                data-target="LukerWay-cacao" class="LukerWay-barLast"></span></div>
     </section>
     <section class="LukerWay-cacao">
-        <div class="LukerWay-bar"><span data-target="LukerWay-header" class="LukerWay-barFirst"></span><span data-target="LukerWay-term" class="LukerWay-barLast"></span> </div>
+        <div class="LukerWay-bar"><span data-target="LukerWay-header" class="LukerWay-barFirst"></span><span
+                data-target="LukerWay-term" class="LukerWay-barLast"></span></div>
     </section>
     <section class="LukerWay-term">
-        <div class="LukerWay-bar"><span data-target="LukerWay-cacao" class="LukerWay-barFirst"></span><span  data-target="LukerWay-social"  class="LukerWay-barLast"></span> </div>
+        <div class="LukerWay-bar"><span data-target="LukerWay-cacao" class="LukerWay-barFirst"></span><span
+                data-target="LukerWay-social" class="LukerWay-barLast"></span></div>
     </section>
     <section class="LukerWay-social">
-        <div class="LukerWay-bar"><span data-target="LukerWay-term" class="LukerWay-barFirst"></span><span data-target="LukerWay-business" class="LukerWay-barLast"></span> </div>
+        <div class="LukerWay-bar"><span data-target="LukerWay-term" class="LukerWay-barFirst"></span><span
+                data-target="LukerWay-business" class="LukerWay-barLast"></span></div>
     </section>
     <section class="LukerWay-business">
         <div id="owl-demo" class="owl-carousel">
-            <div><img src="<?php bloginfo('template_url') ?>/assets/images/guacamayas-bg-03.jpg"></div>
-            <div><img src="<?php bloginfo('template_url') ?>/assets/images/guacamayas-bg-02.jpg"></div>
-            <div><img src="<?php bloginfo('template_url') ?>/assets/images/guacamayas-bg-01.jpg"></div>
-            <div><img src="<?php bloginfo('template_url') ?>/assets/images/necocli-bg-05.jpg"></div>
-            <div><img src="<?php bloginfo('template_url') ?>/assets/images/necocli-bg-03.jpg"></div>
+            <div><img src="<?php bloginfo('template_url') ?>/assets/images/guacamayas-bg-03.jpg"> <span>Necoclí, Antioquia</span>
+            </div>
+            <div><img src="<?php bloginfo('template_url') ?>/assets/images/guacamayas-bg-02.jpg"> <span>Necoclí, 550 hectáreas de cacao</span>
+            </div>
+            <div><img src="<?php bloginfo('template_url') ?>/assets/images/guacamayas-bg-01.jpg"> <span>Necoclí: Melina, plátano y árboles de cacao</span>
+            </div>
+            <div><img src="<?php bloginfo('template_url') ?>/assets/images/necocli-bg-05.jpg"> <span>Necoclí: Finca El Rosario</span>
+            </div>
+            <div><img src="<?php bloginfo('template_url') ?>/assets/images/necocli-bg-03.jpg"> <span>Sistema Agroforestal</span>
+            </div>
             <div><img src="<?php bloginfo('template_url') ?>/assets/images/necocli-bg-02.jpg"></div>
             <div><img src="<?php bloginfo('template_url') ?>/assets/images/img-07.jpg"></div>
         </div>
-        <div class="LukerWay-bar"><span data-target="LukerWay-social" class="LukerWay-barFirst"></span><span data-target="LukerWay-scientific" class="LukerWay-barLast"></span> </div>
+        <div class="projectBusiness">
+            <div>
+                <h2>PROYECTOS EMPRESARIALES</h2>
+
+                <p>Hemos desarrollado un modelo de promoción de cacao para los inversionistas con el fin de promover la
+                    cultura del cacao en el país; nuestro objetivo es tener 5.000 hectáreas plantadas en el año
+                    2017.</p>
+                <p>Para más información contactar a:
+                    <a href="jcarroyave@casaluker.com.co">jcarroyave@casaluker.com.co</a></p>
+            </div>
+            <div>
+                <h3>Qué ofrecemos:</h3>
+                <ul>
+                    <li>Auditoría técnica/ Capacitación del personal.</li>
+                    <li>Software para el control de los costos de producción.</li>
+                    <li>Suministro de semillas y material vegetal.</li>
+                    <li>Montaje de viveros.</li>
+                    <li>Modelo financiero.</li>
+                    <li>Información de recursos financieros, fondos o líneas de crédito para los inversionistas.</li>
+                </ul>
+            </div>
+        </div>
+        <div class="gallery">
+            GALERÍA DE FOTOS
+        </div>
+        <div class="LukerWay-bar"><span data-target="LukerWay-social" class="LukerWay-barFirst"></span><span
+                data-target="LukerWay-scientific" class="LukerWay-barLast"></span></div>
     </section>
     <section class="LukerWay-scientific">
         <article class="LukerWay-scientificContent">
@@ -76,7 +111,8 @@
                 </p>
             </div>
         </article>
-        <div class="LukerWay-bar">NOTICIAS<span  data-target="LukerWay-business" class="LukerWay-barFirst"></span><span  data-target="socialNetwork" class="LukerWay-barLast"></span> </div>
+        <div class="LukerWay-bar">NOTICIAS<span data-target="LukerWay-business" class="LukerWay-barFirst"></span><span
+                data-target="socialNetwork" class="LukerWay-barLast"></span></div>
     </section>
     <section class="socialNetwork">
         <ul>
@@ -85,10 +121,11 @@
             <li><h3>YOUTUBE</h3></li>
         </ul>
     </section>
-
-
 </main>
 <?php wp_enqueue_style('luker-way', get_template_directory_uri() . '/assets/css/owl.carousel.css'); ?>
+<?php wp_enqueue_style('luker-way1', get_template_directory_uri() . '/assets/css/jquery.cover.css'); ?>
 <?php wp_enqueue_script('luker-way', get_template_directory_uri() . '/assets/js/owl.carousel.js', array('jquery'), 1, false); ?>
+<?php wp_enqueue_script('luker-wayCover', get_template_directory_uri() . '/assets/js/jquery.cover.js', array('jquery'), 1, false); ?>
 <?php wp_enqueue_script('luker-way1', get_template_directory_uri() . '/assets/js/lukerWay.js', array('jquery'), 1, false); ?>
+
 <?php get_footer(); ?>
