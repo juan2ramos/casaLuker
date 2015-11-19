c91c9b4bda1fd3d8921a31769949e93c1b04d088
juan2ramos <juan2ramos@gmail.com>
2015-11-19 09:40:55 -0500

    lang functions

--- wp-content/themes/luker/inc/Products.php
+++ wp-content/themes/luker/inc/Products.php
@@ -6,31 +6,31 @@ if ($currentlang == "en-US"){
 ?>
 <ul class="Product">
     <li>
-        <a href="<?php bloginfo('url').'/'.$url ?>/luker-1906">
+        <a href="<?php bloginfo('url'); echo '/'.$url ?>/luker-1906">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-1906.png" alt="">
             <span><?php _e('Luker 1906, Single Origin Chocolates', 'luker'); ?></span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url').'/'.$url ?>/luker-cacao">
+        <a href="<?php bloginfo('url'); echo '/'.$url ?>/luker-cacao">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-cacao.png" alt="">
             <span><?php _e('Luker Cacao, Fino de Aroma Blends', 'luker'); ?></span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url').'/'.$url ?>/luker-maracas">
+        <a href="<?php bloginfo('url'); echo '/'.$url ?>/luker-maracas">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-maracas.png" alt="">
             <span><?php _e('Chocolate Dragees', 'luker'); ?>Grageas de Chocolate</span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url').'/'.$url ?>/luker-derivados">
+        <a href="<?php bloginfo('url'); echo '/'.$url ?>/luker-derivados">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker.png" alt="">
             <span><?php _e('Cocoa derivatives', 'luker'); ?></span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url').'/'.$url ?>/cobertura-de-chocolate">
+        <a href="<?php bloginfo('url'); echo '/'.$url ?>/cobertura-de-chocolate">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/coberturas-sabor-a-chocolate.png"
                  alt="">
             <span><?php _e('Compound', 'luker'); ?></span>
