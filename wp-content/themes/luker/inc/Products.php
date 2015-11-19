ac5abede79d2507533911bab9819dd03c8ed6b74
juan2ramos <juan2ramos@gmail.com>
2015-11-19 07:59:36 -0500

    lang functions

--- wp-content/themes/luker/inc/Products.php
+++ wp-content/themes/luker/inc/Products.php
@@ -1,12 +1,12 @@
 <ul class="Product">
     <li>
-        <a href="<?php bloginfo('url') ?>/luker-1906">
+        <a href="<?php bloginfo('url') ?>/productos/luker-1906">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-1906.png" alt="">
             <span><?php _e('Luker 1906, Single Origin Chocolates','luker'); ?></span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url') ?>/luker-cacao">
+        <a href="<?php bloginfo('url') ?>/productos/luker-cacao">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-cacao.png" alt="">
             <span><?php _e('Luker Cacao, Fino de Aroma Blends','luker'); ?></span>
         </a>
@@ -18,13 +18,13 @@
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url') ?>/luker-derivados">
+        <a href="<?php bloginfo('url') ?>/productos/luker-derivados">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker.png" alt="">
             <span><?php _e('Cocoa derivatives','luker'); ?></span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url') ?>/cobertura-de-chocolate">
+        <a href="<?php bloginfo('url') ?>/productos/cobertura-de-chocolate">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/coberturas-sabor-a-chocolate.png" alt="">
             <span><?php _e('Compound','luker'); ?></span>
         </a>
