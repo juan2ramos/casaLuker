b347392102fdb37868089fa9cb19e8859df2e816
Juan Ramos <juan2ramos@gmail.com>
2015-11-19 13:35:46 -0500

    url

--- wp-content/themes/luker/header.php
+++ wp-content/themes/luker/header.php
@@ -142,7 +142,7 @@ Theme: Luker
 
     <div class="More" id="down">
         <a href="#">
-            <span class="More-view"><?php _e('READ +','luker'); ?>VER +</span>
+            <span class="More-view"><?php _e('READ +','luker'); ?></span>
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px"
                  y="0px" viewBox="0 0 842 595" enable-background="new 0 0 842 595">
                 <defs>
