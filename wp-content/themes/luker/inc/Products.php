652e6f15dae0074ba0e717cfe9fc3399d11eace8
juan2ramos <juan2ramos@gmail.com>
2015-11-19 10:11:52 -0500

    lang functions

--- wp-content/themes/luker/inc/Products.php
+++ wp-content/themes/luker/inc/Products.php
@@ -1,36 +1,39 @@
 <?php
 $currentlang = get_bloginfo('language');
-if ($currentlang == "en-US"){
-    $url = 'product';
-}
+
+$url = array(
+    "en-US" => 'product',
+    "es-CO" => 'productos'
+);
+
 ?>
 <ul class="Product">
     <li>
-        <a href="<?php bloginfo('url'); echo '/'.$url ?>/luker-1906">
+        <a href="<?php bloginfo('url'); echo '/'.$url[$currentlang] ?>/luker-1906">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-1906.png" alt="">
             <span><?php _e('Luker 1906, Single Origin Chocolates', 'luker'); ?></span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url'); echo '/'.$url ?>/luker-cacao">
+        <a href="<?php bloginfo('url'); echo '/'.$url[$currentlang] ?>/luker-cacao">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-cacao.png" alt="">
             <span><?php _e('Luker Cacao, Fino de Aroma Blends', 'luker'); ?></span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url'); echo '/'.$url ?>/luker-maracas">
+        <a href="<?php bloginfo('url'); echo '/'.$url[$currentlang] ?>/luker-maracas">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker-maracas.png" alt="">
             <span><?php _e('Chocolate Dragees', 'luker'); ?>Grageas de Chocolate</span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url'); echo '/'.$url ?>/luker-derivados">
+        <a href="<?php bloginfo('url'); echo '/'.$url[$currentlang] ?>/luker-derivados">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/luker.png" alt="">
             <span><?php _e('Cocoa derivatives', 'luker'); ?></span>
         </a>
     </li>
     <li>
-        <a href="<?php bloginfo('url'); echo '/'.$url ?>/cobertura-de-chocolate">
+        <a href="<?php bloginfo('url'); echo '/'.$url[$currentlang] ?>/cobertura-de-chocolate">
             <img src="<?php bloginfo('template_url') ?>/assets/images/productos/coberturas-sabor-a-chocolate.png"
                  alt="">
             <span><?php _e('Compound', 'luker'); ?></span>
