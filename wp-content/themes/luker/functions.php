8516a270f2be8894c1b9c01eae51086336aa2a21
juan2ramos <juan2ramos@gmail.com>
2015-11-19 08:48:44 -0500

    lang functions

--- wp-content/themes/luker/functions.php
+++ wp-content/themes/luker/functions.php
@@ -131,7 +131,7 @@ function my_page_template_redirect()
         'luker-1906-es' => 'page-luker-1906',
         'the-luker-way-es' => 'page-the-luker-way',
         'product' => 'page-productos',
-        'news' => 'noticias'
+        'news' => 'page-noticias'
 
     );
     if(array_key_exists( get_post($post)->post_name , $arrayUrl) ){
