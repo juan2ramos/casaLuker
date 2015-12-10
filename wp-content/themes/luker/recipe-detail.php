6dd6aeabb0725a74a699298983ddb12b49b0ec48
Juan2Ramos <juan2ramos>
2015-12-10 13:23:53 -0500

    tem recipe

--- wp-content/themes/luker/recipe-detail.php
+++ wp-content/themes/luker/recipe-detail.php
@@ -147,7 +147,26 @@ $url = implode('/', $url);
         $liData.each(function(i){
             var nm = $( this).find('.nm'),
                 ms = $( this).find('.ms'),
-                number =  nm.text();
+
+                ct = $( this).find('.ct'),
+                mt = $( this).find('.mt'),
+
+                number =  nm.text(),
+                numberT =  nm.text();
+
+            if(!isNaN(parseInt(numberT))){
+                if(measure){
+                    ms.text('ºF')
+                    numberT = 1.8 * numberT + 32 ;
+                    numberT = numberT.toFixed(3);
+                }else{
+                    numberT = (numberT - 32)/1.8;
+                    ms.text('ºC')
+                    numberT = numberT.toFixed();
+                }
+                nm.text(number)
+                mt.text(numberT)
+            }
 
             if(!isNaN(parseInt(number))){
                 if(measure){
@@ -162,13 +181,12 @@ $url = implode('/', $url);
                 }
 
                 nm.text(number)
-
-                console.log(number)
             }
         });
 
         measure = (measure)?false:true;
     })
+
 </script>
 </body>
 </html>
